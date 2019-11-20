9900fd202b244e05518aff324eab7283aedb458d
Mahdi Seyyedan <seyedmahdiseydan78@yahoo.com>
2019-11-20 15:17:42 +0330

    add login page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- <div id="nav"> -->
+    <!--   <router&#45;link to="/">Home</router&#45;link> | -->
+    <!--   <router&#45;link to="/about">About</router&#45;link> -->
+    <!-- </div> -->
     <router-view />
   </div>
 </template>
 
 <style>
+html {
+  direction: rtl;
+  line-height: 1.5;
+  height: 100%;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  margin: 0;
+  height: 100%;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  height: 100%;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="login-page">
+    <div class="login">
+      <div class="site-info">
+        <h1>ایشن</h1>
+        <small>شرکت دانش و فناوری اینده روشن</small>
+        <span>i-shan.ir</span>
+      </div>
+      <div class="login-form">
+        <form>
+          <div class="input-control">
+            <input type="text" placeholder="نام کاربری" />
+          </div>
+          <div class="input-control">
+            <input type="text" placeholder="رمز عبور" />
+          </div>
+          <button class="login-button">ورود</button>
+          <a href="#" class="forget-password"
+            >رمز عبور خود را فراموش کرده اید؟
+          </a>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.login-page {
+  background-color: #1b202eff;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: #fff;
+}
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+}
+.login {
+  width: 100%;
+  max-width: 520px;
+  margin: auto;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.login-form input {
+  background-color: transparent;
+  border: 0;
+  border-bottom: 2px solid #424243ff;
+  padding: 5px;
+}
+
+.login-form form {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.login-button {
+  display: block;
+  border-radius: 10px;
+  background-color: #43b649ff;
+  border: 0;
+  color: #fff;
+  padding: 5px 40px;
+}
+
+a.forget-password {
+  color: #ee7338ff;
+  text-decoration: none;
+}
+</style>
