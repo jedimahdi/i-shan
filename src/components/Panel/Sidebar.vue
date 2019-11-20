c896f6b090d4cb31f192d015de860e88f22aca19
Mahdi Seyyedan <seyedmahdiseydan78@yahoo.com>
2019-11-20 20:35:45 +0330

    add icons to sidebar

--- src/components/Panel/Sidebar.vue
+++ src/components/Panel/Sidebar.vue
@@ -3,12 +3,24 @@
     <div class="logo">Logo</div>
     <nav class="sidebar-nav">
       <ul>
-        <li><a href="#">خانه</a></li>
-        <li><a href="#">نمرات</a></li>
-        <li><a href="#">تنظیمات</a></li>
-        <!--        <li><a href="#">تماس با ما</a></li>-->
-        <!--        <li><a href="#">صندوق پیام</a></li>-->
-        <!--        <li><a href="#">دانلود</a></li>-->
+        <li class="active">
+          <a href="#"><span class="lnr lnr-home"></span>خانه</a>
+        </li>
+        <li>
+          <a href="#"><span class="lnr lnr-license"></span>نمرات</a>
+        </li>
+        <li>
+          <a href="#"><span class="lnr lnr-cog"></span>تنظیمات</a>
+        </li>
+        <li>
+          <a href="#"><span class="lnr lnr-phone-handset"></span>تماس با ما</a>
+        </li>
+        <li>
+          <a href="#"><span class="lnr lnr-inbox"></span>صندوق پیام</a>
+        </li>
+        <li>
+          <a href="#"><span class="lnr lnr-download"></span>دانلود</a>
+        </li>
       </ul>
     </nav>
   </aside>
@@ -44,16 +56,28 @@ export default {
 .sidebar-nav ul li:hover {
   background-color: #2e3342ff;
 }
+
+.sidebar-nav ul li.active a {
+  color: #42b547ff;
+}
 .sidebar-nav ul li a {
   display: block;
   text-decoration: none;
   color: #fff;
-  padding: 20px;
+  padding: 20px 0;
+  font-size: 16px;
 }
 
 .logo {
   background-color: #43b649ff;
   text-align: center;
   padding: 20px 32px;
+}
+
+.sidebar-nav .lnr {
+  display: block;
+  padding-bottom: 5px;
+  font-size: 28px;
+  font-weight: 100;
 }
 </style>
