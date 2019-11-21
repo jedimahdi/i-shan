6e612a3a2c241da9b7e32e74f051c01e0c3b91cd
Mahdi Seyyedan <seyedmahdiseydan78@yahoo.com>
2019-11-21 16:52:46 +0330

    add and style contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="contact">
-    Contact us
+    <div class="contact-form">
+      <h4>با ما در تماس باشید</h4>
+      <small>نظرات و پیشنهادات خود را بیان بفرمایید</small>
+      <form action="">
+        <input type="text" placeholder="نام و نام خانوادگی" />
+        <input type="text" placeholder="ایمیل" />
+        <textarea
+          name="comment"
+          id="comment"
+          cols="30"
+          rows="10"
+          placeholder="نظرات و پیشنهادات"
+        ></textarea>
+        <button>ارسال</button>
+      </form>
+    </div>
+    <div class="contact-info">
+      <h5>تماس با ما</h5>
+      <ul>
+        <li>
+          <span class="lnr lnr-map-marker"></span>
+          <span> ستارخان, جنب نشاط</span>
+        </li>
+        <li>
+          <span class="lnr lnr-envelope"></span> <span>i_shan@gmail.com </span>
+        </li>
+        <li><span class="lnr lnr-phone"></span> <span>۰۲۱۶۶۹۲۵۷۲۴ </span></li>
+        <li><span class="lnr lnr-earth"></span> <span>i-shan.com</span></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -10,4 +39,102 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.contact {
+  background-color: #f3f3f5ff;
+  width: 600px;
+  border-radius: 20px;
+  box-shadow: 0 2px 6px rgba(10, 10, 10, 0.2);
+  padding: 50px 50px 50px 0;
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  position: relative;
+  top: 100px;
+}
+
+.contact-form {
+  text-align: center;
+}
+
+.contact-form h4 {
+  color: #42b547ff;
+  font-size: 24px;
+}
+
+.contact-form small {
+  color: #42b547ff;
+  font-size: 18px;
+  font-weight: 300;
+}
+
+.contact-form input {
+  display: block;
+  border: 0;
+  border-radius: 10px;
+  padding: 5px 15px;
+  text-align: center;
+  background-color: #e3e3e3ff;
+  font-size: 16px;
+  margin: 20px auto;
+}
+
+.contact-form input::placeholder,
+.contact-form textarea::placeholder {
+  color: #c4c4c4ff;
+}
+
+.contact-form textarea {
+  text-align: center;
+  border: 0;
+  background-color: #e3e3e3ff;
+  border-radius: 10px;
+  font-size: 16px;
+}
+
+.contact-form button {
+  display: block;
+  margin: auto;
+  border: 0;
+  background-color: #42b547ff;
+  color: #fff;
+  padding: 5px 25px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 10px;
+}
+
+.contact-info {
+  background-color: #191e2eff;
+  border-radius: 20px;
+  padding: 50px;
+  margin-left: -30px;
+}
+
+.contact-info h5 {
+  color: #fff;
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 50px;
+}
+
+.contact-info ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  color: #a5a6abff;
+}
+.contact-info ul li {
+  display: flex;
+}
+.contact-info ul li span {
+  font-size: 17px;
+}
+
+.contact-info ul li span.lnr {
+  font-size: 24px;
+  padding-bottom: 25px;
+  padding-left: 15px;
+}
+</style>
