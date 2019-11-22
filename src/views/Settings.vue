20099d1af0abd391406187a6a1310388eb141eab
Mahdi Seyyedan <seyedmahdiseydan78@yahoo.com>
2019-11-22 16:19:55 +0330

    complete settings page ui

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,27 +4,57 @@
       <h4>تنظیمات ضاهر</h4>
       <Box>
         <SettingsOption>
-          رنگ بندی
+          <span>رنگ بندی</span>
           <template v-slot:options>
             <option value="">تم شماره یک</option>
           </template>
         </SettingsOption>
         <SettingsOption>
-          فونت
+          <span>فونت</span>
           <template v-slot:options>
             <option value="">ایران یکان</option>
           </template>
         </SettingsOption>
         <SettingsOption>
-          اندازه فونت
+          <span>اندازه فونت</span>
           <template v-slot:options>
             <option value="">متوسط</option>
           </template>
         </SettingsOption>
         <SettingsOption>
-          زوم صفحه
+          <span>زوم صفحه</span>
           <template v-slot:options>
             <option value="">صد در صد</option>
+          </template>
+        </SettingsOption>
+      </Box>
+    </div>
+
+    <div class="setting-group">
+      <h4>دانلود خودکار</h4>
+      <Box>
+        <SettingsOption>
+          <span>دوره تکرار</span>
+          <template v-slot:options>
+            <option value="">هفتگی</option>
+          </template>
+        </SettingsOption>
+        <SettingsOption>
+          <span>روز هفته</span>
+          <template v-slot:options>
+            <option value="">دوشنبه</option>
+          </template>
+        </SettingsOption>
+        <SettingsOption>
+          <span>زمان شروع دانلود</span>
+          <template v-slot:options>
+            <option value="">1398/07/01</option>
+          </template>
+        </SettingsOption>
+        <SettingsOption>
+          <span>زمان پایان دانلود</span>
+          <template v-slot:options>
+            <option value="">1398/07/23</option>
           </template>
         </SettingsOption>
       </Box>
@@ -49,5 +79,11 @@ export default {
 .settings {
   width: 800px;
   margin: auto;
+}
+
+.setting-group h4 {
+  font-size: 28px;
+  font-weight: 400;
+  padding: 25px;
 }
 </style>
