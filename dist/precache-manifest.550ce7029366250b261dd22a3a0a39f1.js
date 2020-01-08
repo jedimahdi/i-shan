87c5be14291b203cd2b404b7bddbf10ccdc03309
jedimahdi <seyedmahdiseydan78@yahoo.com>
2020-01-08 16:03:32 +0330

    add dist folder

---
+++ dist/precache-manifest.550ce7029366250b261dd22a3a0a39f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66996ce0c5b514dfa086",
+    "url": "/css/app.a4777d29.css"
+  },
+  {
+    "revision": "5edc4ef3a11da03658767f1303af658e",
+    "url": "/img/logo.5edc4ef3.svg"
+  },
+  {
+    "revision": "da55cca0a3e2fef9999a3af02474dc8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66996ce0c5b514dfa086",
+    "url": "/js/app.b084c238.js"
+  },
+  {
+    "revision": "8857ce8574be85c8aac5",
+    "url": "/js/chunk-vendors.92c56b17.js"
+  },
+  {
+    "revision": "45d40a9adc97016ba7afe97df09a4700",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
